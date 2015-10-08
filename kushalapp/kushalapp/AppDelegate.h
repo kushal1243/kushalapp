e7df8ec8a806747c50d0169b4cbdf8d7a62adae6
apple <apple@unknown.local>
2015-10-08 17:13:51 +0530

    first push

---
+++ kushalapp/kushalapp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  kushalapp
+//
+//  Created by apple on 08/10/15.
+//  Copyright © 2015 kushal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ kushalapp/kushalapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kushalapp
+//
+//  Created by apple on 08/10/15.
+//  Copyright © 2015 kushal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
