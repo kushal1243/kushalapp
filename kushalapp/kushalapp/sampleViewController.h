c9cb59bf284e654b9d01398427fb39a584a2ed43
Kushal Mandala <kushalmandala@Kushals-MacBook-Pro.local>
2019-06-10 19:40:39 +0530

    second commit

---
+++ kushalapp/kushalapp/sampleViewController.h
@@ -0,0 +1,17 @@
+//
+//  sampleViewController.h
+//  kushalapp
+//
+//  Created by Kushal Mandala on 10/06/19.
+//  Copyright © 2019 kushal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface sampleViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ kushalapp/kushalapp/sampleViewController.m
@@ -0,0 +1,32 @@
+//
+//  sampleViewController.m
+//  kushalapp
+//
+//  Created by Kushal Mandala on 10/06/19.
+//  Copyright © 2019 kushal. All rights reserved.
+//
+
+#import "sampleViewController.h"
+
+@interface sampleViewController ()
+
+@end
+
+@implementation sampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
